c881ebbe3c7e2bc0dd2a880c373f60aaa22b0c26
Lennard, Claproth <lennardclaproth@gmail.com>
2025-07-23 08:14:57 +0200

    Removed unused mappings

--- dotnet/src/PopcornMarket.FinancialAtlas.Application/Mappings/PeriodTypeMappingProfile.cs
+++ dotnet/src/PopcornMarket.FinancialAtlas.Application/Mappings/PeriodTypeMappingProfile.cs
@@ -10,14 +10,4 @@ public class PeriodTypeMappingProfile : Profile
         CreateMap<Contracts.Enums.PeriodType, PeriodType>();
         CreateMap<PeriodType, Contracts.Enums.PeriodType>();
     }
-    
-    // private static PeriodType MapToDomain(Contracts.Enums.PeriodType dto) =>
-    // dto switch
-    // {
-    //     Contracts.Enums.PeriodType.Yearly => PeriodType.Yearly,
-    //     Contracts.Enums.PeriodType.HalfYearly => PeriodType.HalfYearly,
-    //     Contracts.Enums.PeriodType.Quarterly => PeriodType.Quarterly,
-    //     _ => throw new ArgumentOutOfRangeException(nameof(dto), $"Unsupported PeriodType: {dto}")
-    // };
-
 }
